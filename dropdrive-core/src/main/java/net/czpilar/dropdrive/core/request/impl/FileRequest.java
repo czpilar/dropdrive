0a7103b7e0588189f77a2acbb52c897ec9d240dc
czpilar <david@czpilar.net>
2015-06-29 17:20:24 +0200

    added chunk upload retries

--- dropdrive-core/src/main/java/net/czpilar/dropdrive/core/request/impl/FileRequest.java
+++ dropdrive-core/src/main/java/net/czpilar/dropdrive/core/request/impl/FileRequest.java
@@ -10,6 +10,8 @@ import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxWriteMode;
 import net.czpilar.dropdrive.core.listener.IFileUploadProgressListener;
 import net.czpilar.dropdrive.core.request.IFileRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Template class implementing file request using chunk file upload.
@@ -18,7 +20,10 @@ import net.czpilar.dropdrive.core.request.IFileRequest;
  */
 public abstract class FileRequest implements IFileRequest {
 
+    private static final Logger LOG = LoggerFactory.getLogger(FileRequest.class);
+
     public static final int CHUNK_SIZE = 4194304; // 4MB
+    public static final int CHUNK_RETRIES = 3;
 
     public static class Insert extends FileRequest {
         public Insert(DbxClient dbxClient, String remoteFilePath, File localFile) {
@@ -52,32 +57,52 @@ public abstract class FileRequest implements IFileRequest {
 
     @Override
     public DbxEntry.File execute() throws IOException, DbxException {
-        int offset = 0;
+        int offsetBytes = 0;
 
-        progress(IFileUploadProgressListener.State.INITIATION, offset);
+        progress(IFileUploadProgressListener.State.INITIATION, offsetBytes);
 
         FileInputStream stream = new FileInputStream(localFile);
 
         byte[] data = new byte[CHUNK_SIZE];
         try {
             String chunkId = null;
-            int read;
-            while ((read = stream.read(data)) != -1) {
-                if (offset == 0) {
-                    progress(IFileUploadProgressListener.State.IN_PROGRESS, offset);
-                    chunkId = dbxClient.chunkedUploadFirst(data, 0, read);
-                } else {
-                    dbxClient.chunkedUploadAppend(chunkId, offset, data, 0, read);
-                }
-                offset += read;
-                progress(IFileUploadProgressListener.State.IN_PROGRESS, offset);
+            int readBytes;
+            while ((readBytes = stream.read(data)) != -1) {
+                chunkId = uploadChunkWithRetries(offsetBytes, data, chunkId, readBytes);
+                offsetBytes += readBytes;
+                progress(IFileUploadProgressListener.State.IN_PROGRESS, offsetBytes);
             }
             DbxEntry.File file = dbxClient.chunkedUploadFinish(remoteFilePath, dbxWriteMode, chunkId);
-            progress(IFileUploadProgressListener.State.COMPLETE, offset);
+            progress(IFileUploadProgressListener.State.COMPLETE, offsetBytes);
             return file;
         } finally {
             stream.close();
         }
+    }
+
+    private String uploadChunkWithRetries(int offsetBytes, byte[] data, String chunkId, int readBytes) throws DbxException {
+        int retry = 0;
+        while (true) {
+            try {
+                return uploadChunk(offsetBytes, data, chunkId, readBytes);
+            } catch (DbxException e) {
+                retry++;
+                if (retry > CHUNK_RETRIES) {
+                    throw e;
+                }
+                LOG.warn("Error during executing uploading chunk file, offset bytes {}, retrying for {} time(s), message: {}", offsetBytes, retry, e.getMessage());
+            }
+        }
+    }
+
+    private String uploadChunk(int offset, byte[] data, String chunkId, int read) throws DbxException {
+        if (offset == 0) {
+            progress(IFileUploadProgressListener.State.IN_PROGRESS, offset);
+            chunkId = dbxClient.chunkedUploadFirst(data, 0, read);
+        } else {
+            dbxClient.chunkedUploadAppend(chunkId, offset, data, 0, read);
+        }
+        return chunkId;
     }
 
     private void progress(IFileUploadProgressListener.State state, long uploaded) {
